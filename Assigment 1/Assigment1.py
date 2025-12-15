08c5619095c3dbb8368283e239e2bab8441bea30
MehdiHassanSepai21032004 <mehdihassansepai@gmail.com>
2025-12-14 23:37:57 -0800

    Add files via upload

---
+++ Assigment 1/Assigment1.py
@@ -0,0 +1,24 @@
+a = int(input("Enter the first number: " ))
+b = int(input("Enter the first number: "  ))
+
+c = (a+b)
+
+print("Addition:",c)
+
+d = (a-b)
+
+print("Subtraction:",d)
+
+e = (a*b)
+
+print("Multiplication:", e)
+
+f = (a/b)
+
+print("Division:",f)
+
+a = input("Enter Your First Name: " )
+
+b = input("Enter Your Last Name: " )
+
+print ("Hello,",a + " " + b+"!", "Welcome to the Python Program.")
